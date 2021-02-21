d0c1c20d2a4b14195d8728703924c04a3272ae45
海 稲垣 <71507@toyota.kosen-ac.jp>
2021-02-21 16:37:01 +0900

    music

--- makingFPS/Main.cpp
+++ makingFPS/Main.cpp
@@ -43,13 +43,13 @@ public:
 class Player {
 public:
 	Vec2 pos;
-	double theta = (std::numbers::pi/8)*4;
+	double theta = (std::numbers::pi / 8) * 4;
 	double vel = 1;//1
 	Eye eye;
-	double turnvel = std::numbers::pi /80 ;//80
-  std::vector<Line> map_;
+	double turnvel = std::numbers::pi / 80;//80
+	std::vector<Line> map_;
 
-	Player(Vec2 p, const std::vector<Line>& map) : pos(p.x, p.y), eye(pos, theta) ,map_(map) {}
+	Player(Vec2 p, const std::vector<Line>& map) : pos(p.x, p.y), eye(pos, theta), map_(map) {}
 
 	void update() {
 
@@ -62,7 +62,7 @@ public:
 			pos.x -= vel * std::cos(theta);
 			pos.y -= vel * std::sin(theta);
 		}
-		if (std::any_of(map_.cbegin(), map_.cend(), [this](const auto& a){ return a.intersects(pos); })) pos = tmp_pos;
+		if (std::any_of(map_.cbegin(), map_.cend(), [this](const auto& a) { return a.intersects(pos); })) pos = tmp_pos;
 
 		if (KeyRight.pressed()) {
 			theta += turnvel;
@@ -79,15 +79,15 @@ public:
 	}
 };
 
-std::vector<std::optional<std::pair<Vec2,double>>> makefocus(const Player& Player,
-	 const std::vector<Line>& walls) {
+std::vector<std::optional<std::pair<Vec2, double>>> makefocus(const Player& Player,
+	const std::vector<Line>& walls) {
 
 	const Vec2 light = Player.pos;
 
-	std::vector<std::optional<std::pair<Vec2,double>>> focus;
+	std::vector<std::optional<std::pair<Vec2, double>>> focus;
 	for (const auto& l : Player.eye.lines) {
 		const auto& line = l.first;
-	  std::vector<std::pair<Vec2,double>> tmpfocus;
+		std::vector<std::pair<Vec2, double>> tmpfocus;
 		for (const auto& wall : walls) {
 			if (wall.intersectsAt(line).has_value()) {
 				const auto pos = wall.intersectsAt(line).value().asPoint();
@@ -105,7 +105,7 @@ std::vector<std::optional<std::pair<Vec2,double>>> makefocus(const Player& Playe
 			auto itr = std::min_element(tmpfocus.cbegin(), tmpfocus.cend(),
 				[&Player](const auto& a, const auto& b) {
 					return	Geometry2D::Distance(a.first, Player.pos) <
-							Geometry2D::Distance(b.first, Player.pos);
+						Geometry2D::Distance(b.first, Player.pos);
 				});
 			focus.push_back(*itr);
 		}
@@ -113,7 +113,7 @@ std::vector<std::optional<std::pair<Vec2,double>>> makefocus(const Player& Playe
 	return focus;
 }
 
-void drawFPSview(const std::vector<std::optional<std::pair<Vec2, double>>>& focus, const Player& Player,double cellsize,int height,int width) {
+void drawFPSview(const std::vector<std::optional<std::pair<Vec2, double>>>& focus, const Player& Player, double cellsize, int height, int width) {
 	for (int i = 0; i < Player.eye.lines.size(); i++) {
 		const double window_width = Window::ClientSize().x;
 		const double window_height = Window::ClientSize().y;
@@ -127,12 +127,12 @@ void drawFPSview(const std::vector<std::optional<std::pair<Vec2, double>>>& focu
 			constexpr auto wall_height = 5000;
 			int a = focus[i].value().first.y;
 			int b = focus[i].value().first.x;
-			if (a<=cellsize*2&&b<=cellsize*2) {
+			if (a <= cellsize * 2 && b <= cellsize * 2) {
 				Line(window_width / 4 + tmp, window_height / 2 - wall_height / dist,
 					window_width / 4 + tmp, window_height / 2 + wall_height / dist)
 					.draw(8, HSV(241, 1, focus[i].value().second));
 			}
-			else if (a>=cellsize*(height-2)&&b>=cellsize*(width-2)) {
+			else if (a >= cellsize * (height - 2) && b >= cellsize * (width - 2)) {
 				Line(window_width / 4 + tmp, window_height / 2 - wall_height / dist,
 					window_width / 4 + tmp, window_height / 2 + wall_height / dist)
 					.draw(8, Palette::Red, HSV(0, 1, focus[i].value().second));
@@ -140,15 +140,16 @@ void drawFPSview(const std::vector<std::optional<std::pair<Vec2, double>>>& focu
 			else if (a % static_cast<int>(cellsize) == 0 && b % static_cast<int>(cellsize) == 0) {
 				Line(window_width / 4 + tmp, window_height / 2 - wall_height / dist,
 					window_width / 4 + tmp, window_height / 2 + wall_height / dist)
-					.draw(8,Palette::Orange, HSV(32, 82, focus[i].value().second));
+					.draw(8, Palette::Orange, HSV(32, 82, focus[i].value().second));
 			}
 			else {
 				Line(window_width / 4 + tmp, window_height / 2 - wall_height / dist,
 					window_width / 4 + tmp, window_height / 2 + wall_height / dist)
-					.draw(8,HSV(0, 0, focus[i].value().second));
+					.draw(8, HSV(0, 0, focus[i].value().second));
 			}
 		}
 	}
+	if (std::hypot(cellsize * (width - 2) - Player.pos.x, cellsize * (height - 2) - Player.pos.y) < cellsize) {}
 }
 
 std::vector<Line> makemap(int height, int width, double cell_size) {
@@ -162,7 +163,7 @@ std::vector<Line> makemap(int height, int width, double cell_size) {
 	}
 
 	enum class direction {
-		up, 
+		up,
 		down,
 		right,
 		left,
@@ -173,72 +174,76 @@ std::vector<Line> makemap(int height, int width, double cell_size) {
 	int digg_pos_y = 1;
 	map[digg_pos_y][digg_pos_x] = 0;
 	std::unordered_map<direction, bool> end_flags;
-	for (auto dir = static_cast<int>(direction::up); dir < static_cast<int>(direction::size); ++dir) end_flags[static_cast<direction>(dir)] = false; 
+	for (auto dir = static_cast<int>(direction::up); dir < static_cast<int>(direction::size); ++dir) end_flags[static_cast<direction>(dir)] = false;
 
 	int a = 0;
-	while(a<10000) {
+	while (a < 10000) {
 		a++;
 		const auto dir = static_cast<direction>(Random(0, 3));
 
 		switch (dir) {
 		case direction::up:
-		  digg_pos_y -= 2;
+			digg_pos_y -= 2;
 			if (digg_pos_y > 0 && map[digg_pos_y][digg_pos_x] == 1) {
-			  map[digg_pos_y][digg_pos_x] = 0;
+				map[digg_pos_y][digg_pos_x] = 0;
 				map[digg_pos_y + 1][digg_pos_x] = 0;
 				for (auto& e : end_flags) e.second = false;
-			} else {
+			}
+			else {
 				end_flags[direction::up] = true;
 				digg_pos_y += 2;
 			}
 			break;
 		case direction::down:
 			digg_pos_y += 2;
-			if (digg_pos_y < height&& map[digg_pos_y][digg_pos_x] == 1) {
+			if (digg_pos_y < height && map[digg_pos_y][digg_pos_x] == 1) {
 				map[digg_pos_y][digg_pos_x] = 0;
 				map[digg_pos_y - 1][digg_pos_x] = 0;
-			  for (auto& e : end_flags) e.second = false;
-			} else {
+				for (auto& e : end_flags) e.second = false;
+			}
+			else {
 				end_flags[direction::down] = true;
 				digg_pos_y -= 2;
 			}
 			break;
 		case direction::right:
 			digg_pos_x += 2;
-			if (digg_pos_x < width&& map[digg_pos_y][digg_pos_x] == 1) {
+			if (digg_pos_x < width && map[digg_pos_y][digg_pos_x] == 1) {
 				map[digg_pos_y][digg_pos_x] = 0;
 				map[digg_pos_y][digg_pos_x - 1] = 0;
 				for (auto& e : end_flags) e.second = false;
-			} else {
+			}
+			else {
 				end_flags[direction::right] = true;
 				digg_pos_x -= 2;
 			}
 			break;
 		case direction::left:
 			digg_pos_x -= 2;
-			if (digg_pos_x  > 0&& map[digg_pos_y][digg_pos_x] == 1) {
+			if (digg_pos_x > 0 && map[digg_pos_y][digg_pos_x] == 1) {
 				map[digg_pos_y][digg_pos_x] = 0;
 				map[digg_pos_y][digg_pos_x + 1] = 0;
 				for (auto& e : end_flags) e.second = false;
-			} else {
-			  end_flags[direction::left] = true;
+			}
+			else {
+				end_flags[direction::left] = true;
 				digg_pos_x += 2;
 			}
 			break;
 		}
-		int Random_x = std::floor(Random(2, width)/2)-1;
-		int Random_y = std::floor(Random(2, height)/2)-1;
-		
-		if (std::all_of(end_flags.cbegin(), end_flags.cend(), [](const auto& e){ return e.second; })&& map[Random_y * 2 + 1][Random_x * 2 + 1]==0) {
-			digg_pos_x = Random_x*2+1;
-			digg_pos_y = Random_y*2+1;
+		int Random_x = std::floor(Random(2, width) / 2) - 1;
+		int Random_y = std::floor(Random(2, height) / 2) - 1;
+
+		if (std::all_of(end_flags.cbegin(), end_flags.cend(), [](const auto& e) { return e.second; }) && map[Random_y * 2 + 1][Random_x * 2 + 1] == 0) {
+			digg_pos_x = Random_x * 2 + 1;
+			digg_pos_y = Random_y * 2 + 1;
 			for (auto& e : end_flags) e.second = false;
 		}
 	}
 
 	for (auto m = map.begin(); m != map.end(); m++) {
-	  for (auto n = m->begin(); n != m->end(); n++) {
-		  if (m == map.begin() || m == map.end() - 1 || n == m->begin() || n == m->end() - 1) *n = 1;
+		for (auto n = m->begin(); n != m->end(); n++) {
+			if (m == map.begin() || m == map.end() - 1 || n == m->begin() || n == m->end() - 1) *n = 1;
 		}
 	}
 
@@ -267,24 +272,22 @@ void drawmap(const std::vector<Line>& walls) {
 void Main() {
 	Window::SetStyle(WindowStyle::Sizable);
 	Scene::SetScaleMode(ScaleMode::ResizeFill);
- 
-  constexpr int height = 39;
-  constexpr int width = 13;
-  const double cell_size = (Window::ClientSize().x / 4) / width;
-
+	constexpr int height = 39;
+	constexpr int width = 13;
+	const double cell_size = (Window::ClientSize().x / 4) / width;
+	const Audio audio(U"nc234276.wav", Arg::loop = true);
 	const auto map = makemap(height, width, cell_size);
 	Player Player({ 20, 20 }, map);
-
+	audio.play();
 	while (System::Update()) {
 		const auto x = Window::ClientSize().x;
 		const auto y = Window::ClientSize().y;
-		Rect(x / 4,0, x, y).draw(Arg::top = Palette::Lightblue, Arg::bottom = Palette::Black);
-		Rect(x/4, y / 2, x, y).draw(Arg::top = Palette::Black, Arg::bottom = Palette::Green);
+		Rect(x / 4, 0, x, y).draw(Arg::top = Palette::Lightblue, Arg::bottom = Palette::Black);
+		Rect(x / 4, y / 2, x, y).draw(Arg::top = Palette::Black, Arg::bottom = Palette::Green);
 
 		Player.update();
 		Player.draw();
 		drawmap(map);
-		drawFPSview(makefocus(Player, map), Player,cell_size,height,width);
-
+		drawFPSview(makefocus(Player, map), Player, cell_size, height, width);
 	}
 }
